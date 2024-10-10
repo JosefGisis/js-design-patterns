c6405535fce8a6355642e2389c6742ba3b18af08
josef gisis <jsquared.gisis@gmail.com>
2024-10-10 10:15:29 -0400

    added command dp

--- src/behavioral/command.js
+++ src/behavioral/command.js
@@ -9,3 +9,60 @@
  */
 
 // We create a user object that has several requests. Invokers will create requests and pass them to the user object.
+function User(name, age, gender) {
+    // Prepended these with an underscore because "apparently" they are defined somewhere else.
+    let _name = name
+    let _gender = gender
+    let _age = age
+
+    return {
+        setName: function (newName) {
+            _name = newName
+        },
+        setGender: function (newGender) {
+            _gender = newGender
+        },
+
+        setAge: function (newAge) {
+            _age = newAge
+        },
+        getInfo: function () {
+            return {
+                _name,
+                _gender,
+                _age,
+            }
+        },
+    }
+}
+
+function SetGenderRequest(user, newGender) {
+    this.user = user
+    this.newGender = newGender
+
+    this.execute = function () {
+        user.setGender(newGender)
+    }
+}
+
+function SetAgeRequest(user, newAge) {
+    this.user = user
+    this.newAge = newAge
+
+    this.execute = function () {
+        user.setAge(newAge)
+    }
+}
+
+// Usage
+const josefGisis = new User('Josef Gisis', 28, 'female')
+
+console.log(josefGisis.getInfo()) // Oops, I am not 28 nor female.
+
+const setGenderRequest = new SetGenderRequest(josefGisis, 'male')
+const setAgeRequest = new SetAgeRequest(josefGisis, 25)
+
+setGenderRequest.execute()
+setAgeRequest.execute()
+
+console.log('\n', josefGisis.getInfo())
