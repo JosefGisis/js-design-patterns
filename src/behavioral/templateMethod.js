59603bc707d3eeb0179884ad48b7840ad6d27684
josef gisis <jsquared.gisis@gmail.com>
2024-10-10 18:01:27 -0400

    added templateMethod dp

--- src/behavioral/templateMethod.js
+++ src/behavioral/templateMethod.js
@@ -6,4 +6,66 @@
  * This pattern is useful when you have an algorithm that has a fixed set of steps but the implementation of those steps can vary.
  *
  * The template method is related the builder patterns in that the builder pattern often uses a template method to define the building process.
+ * 
+ * With prototypes, all prototype methods are essentially hooks; that is, they have a default implementation that can be overridden
+ * by the "subclass if needed". We could leave some prototype methods empty and throw an error if they are called, but what is the
+ * point of prototyping if we don't use its features?
  */
+
+function BakeBread() {}
+BakeBread.prototype = {
+    doMakeBread: function () {
+        this.prepareIngredients()
+        this.mixIngredients()
+        this.kneadDough()
+        this.letDoughRise()
+        this.bakeDough()
+    },
+    prepareIngredients: function () {
+        console.log('Getting flour, water, and yeast')
+    },
+    mixIngredients: function () {
+        console.log('Mixing ingredients')
+    },
+    kneadDough: function () {
+        console.log('Kneading dough')
+    },
+    letDoughRise: function () {
+        console.log('Letting dough rise')
+    },
+    bakeDough: function () {
+        console.log('Baking dough')
+    },
+}
+
+// Set sourdough as the prototype inheritor of bake bread
+function BakeSourdough() {
+    this.prepareIngredients = function () {
+        console.log('Getting flour, water, and sourdough starter')
+    }
+    this.letDoughRise = function () {
+        console.log('Letting dough rise for 24 hours')
+    }
+}
+BakeSourdough.prototype = new BakeBread()
+BakeSourdough.prototype.constructor = BakeSourdough
+
+function BakeWholeWheat() {
+    this.prepareIngredients = function () {
+        console.log('Getting whole wheat flour, water, and yeast')
+    }
+    this.bakeDough = function () {
+        console.log('Baking dough for 40 minutes')
+    }
+}
+BakeWholeWheat.prototype = new BakeBread()
+BakeWholeWheat.prototype.constructor = BakeWholeWheat
+
+// Usage
+const sourdough = new BakeSourdough()
+sourdough.doMakeBread()
+
+console.log('-------------------')
+
+const wholeWheat = new BakeWholeWheat()
+wholeWheat.doMakeBread()
