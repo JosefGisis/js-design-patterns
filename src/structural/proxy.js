ceb2450549350ea52df0df7417dd22e0c6410af1
josef gisis <jsquared.gisis@gmail.com>
2024-10-09 23:13:16 -0400

    added proxy dp

--- src/structural/proxy.js
+++ src/structural/proxy.js
@@ -6,9 +6,43 @@
  *   object being expensive to create.
  * 2. Protection Proxy: controls access to the real object (checks authority or scope).
  * 3. Remote Proxy: provides a local representation of an object that is in a different address.
- *
- * In this example we will use a virtual proxy to simplify the example. The object will be a large
- * photo.
- * We will use a timeout to simulate the time it would take for users to scroll down to the
- * position where the photo is located.
  */
+
+// In this example, the proxy displays a fetching message before the actual data is fetched.
+
+// Mimic a fetch request
+function makeRequest() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                name: 'John Doe',
+                age: 30,
+                email: 'johndoe@gmail.com',
+            })
+        }, 1000)
+    })
+}
+
+// For a more complex example, consider creating a prototype.
+function Proxy() {
+    this.request = function () {
+        console.log('Proxy: Making request...')
+        const data = new Data()
+        data.request()
+    }
+}
+
+// Actually fetch the data
+function Data() {
+    this.request = function () {
+        makeRequest().then((data) => {
+            console.log(data)
+        })
+    }
+}
+
+// Usage
+const proxy = new Proxy()
+proxy.request() // Proxy: Making request...
+// after 1 second
+// { name: 'John Doe', age: 30, email: '
