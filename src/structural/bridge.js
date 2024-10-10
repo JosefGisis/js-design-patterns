768181cf2fc84a0d0a3eae2f61f5de237baa4e5c
josef gisis <jsquared.gisis@gmail.com>
2024-10-10 12:12:04 -0400

    added bridge dp

--- src/structural/bridge.js
+++ src/structural/bridge.js
@@ -11,4 +11,61 @@
  * processor from the payment object and we can easily swap out the payment processor without changing the payment object.
  */
 
-// example payment processor
+// example payment processor. These are the implementations that we want to abstract from the payment object.
+const mastercardProcessor = (function () {
+    return {
+        processPayment: function (amount) {
+            console.log(`Processing ${amount} via Mastercard`)
+        },
+        refundPayment: function (amount) {
+            console.log(`Refunding ${amount} via Mastercard`)
+        },
+    }
+})()
+
+const visaProcessor = (function () {
+    return {
+        processPayment: function (amount) {
+            console.log(`Processing ${amount} via Visa`)
+        },
+        refundPayment: function (amount) {
+            console.log(`Refunding ${amount} via Visa`)
+        },
+    }
+})()
+
+const paypalProcessor = (function () {
+    return {
+        processPayment: function (amount) {
+            console.log(`Processing ${amount} via Paypal`)
+        },
+        refundPayment: function (amount) {
+            console.log(`Refunding ${amount} via Paypal`)
+        },
+    }
+})()
+
+// payment abstraction (also a prototype)
+function Payment(processor, amount) {
+    this.processor = processor
+    this.amount = amount
+}
+Payment.prototype.makePayment = function (amount) {
+    this.processor.processPayment(amount)
+}
+Payment.prototype.refundPayment = function (amount) {
+    this.processor.refundPayment(amount)
+}
+
+// Extend Payment for canadian payments
+function CanadianPayment(processor, amount) {
+    this.processor = processor
+    this.amount = amount * 1.3
+}
+CanadianPayment.prototype = new Payment()
+CanadianPayment.prototype.constructor = CanadianPayment
+
+// Usage
+const canadianPayment = new CanadianPayment(mastercardProcessor)
+canadianPayment.makePayment(100)
+canadianPayment.refundPayment(100)
