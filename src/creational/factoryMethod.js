28f0f998c1996fb7c743d3301aac1daccd7d42e7
josef gisis <jsquared.gisis@gmail.com>
2024-10-14 18:55:16 -0400

    updated factory method

--- src/creational/factoryMethod.js
+++ src/creational/factoryMethod.js
@@ -1,52 +1,51 @@
 /**
- * Factory Method is a creational design pattern that provides an interface for creating objects in a superclass
- * , but allows subclasses to alter the type of objects that will be created. This is important when the particular
+ * Factory Method is a creational design pattern that provides an interface for creating objects in a superclass,
+ * but allows subclasses to alter the type of objects that will be created. This is important when the particular
  * object that will be created is not known. The abstract factory delegate the responsibility of creating objects to
  * its subclasses.
  *
- * JavaScript is not inherently class-based, so we can use function as our factory method.
+ * The intent of factory method is to "Define an interface for creating an object, but let the subclasses decide
+ * which class to instantiate. Factory Method lets a class defer instantiation to subclasses." The intent is a
+ * class based pattern that uses inheritance to create objects. We don't have interface in JavaScript, so we can
+ * use prototypical inheritance. We will have an object that will implement some default instantiation logic, and
+ * then we will have other objects that will inherit from the default object and override the instantiation logic.
  */
 
-function objectNumber1() {
-    this.name = 'objectNumber1'
+function AggregateManager() {
+    this.aggregate = new Array(['hello', 'world', 'how', 'are', 'you'])
 }
-
-function objectNumber2() {
-    this.name = 'objectNumber2'
-}
-
-function objectNumber3() {
-    this.name = 'objectNumber3'
-}
-
-function NameLogger() {
-    this.loggerObject = {}
-}
-NameLogger.prototype = {
-    setLoggerObject: function (object) {
-        throw new Error('This method must be overwritten')
-    },
-    logName: function () {
-        console.log(this.loggerObject.name)
-    },
-}
-
-function FavoriteNumberLogger() {
-    this.loggerObject = {}
-    this.setLoggerObject = function (favoriteNumber) {
-        if (favoriteNumber === 1) {
-            this.loggerObject = new objectNumber1()
-        } else if (favoriteNumber === 2) {
-            this.loggerObject = new objectNumber2()
-        } else if (favoriteNumber === 3) {
-            this.loggerObject = new objectNumber3()
-        } else {
-            throw new Error('Invalid favorite number')
+AggregateManager.prototype.logElements = function () {
+    if (Array.isArray(this.aggregate)) {
+        this.aggregate.forEach((element) => {
+            console.log(element)
+        })
+    } else {
+        for (const key in this.aggregate) {
+            console.log(this.aggregate[key])
         }
     }
 }
-FavoriteNumberLogger.prototype = new NameLogger()
+AggregateManager.prototype.logAggregate = function () {
+    console.log(this.aggregate)
+}
 
-const favoriteNumberObjectLogger = new FavoriteNumberLogger()
-favoriteNumberObjectLogger.setLoggerObject(2)
-favoriteNumberObjectLogger.logName()
+// objectManager adopts the AggregateManager prototype, but uses an object as its aggregate
+// rather than an array.
+const objectManager = Object.create(AggregateManager.prototype, {
+    aggregate: {
+        value: new Object({
+            string1: 'hello',
+            string2: 'world',
+            string3: 'how',
+            string4: 'are',
+            string5: 'you',
+        }),
+        writable: true,
+        enumerable: true,
+        configurable: true,
+    },
+})
+
+// Usage (client)
+objectManager.logElements()
+objectManager.logAggregate()
