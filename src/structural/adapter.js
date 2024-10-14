3354f7101750a7ee642deb68afbe7cd3d538cbba
josef gisis <jsquared.gisis@gmail.com>
2024-10-14 18:58:22 -0400

    added more docs to adapter

--- src/structural/adapter.js
+++ src/structural/adapter.js
@@ -2,6 +2,9 @@
  * The adapter pattern allow an object (the adoptee) to be able to be used with client code that expects a different interface.
  * The adapter can be done through a class which subclasses the adoptee, or it can be done through object composition (in which
  * case a delegate transforms the expected object).
+ * 
+ * In this example we won't use subclassing to adapt an interface, but rather we will use object composition and prototypical
+ * inheritance to adapt the interface of an object.
  *
  * Two-way adapters can be used to easily swap interfaces in both directions.
  * Pluggable adapters are adapters that are contained in the adaptee. This is the case when the adaptee needs to make minimal
